1b702738561f44b118da25197b94af49545ebc5c
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2025-01-17 22:41:04 +0600

    lesson-01

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Welcome to JavaScript!");
+Fullname = "Khairul Hasan Rakib";
+console.log(Fullname);
+
+let age = 20;
+console.log(age);
+
+const country = "Bangladesh";
+console.log(country);
+
+const student = {
+    name: "Khairul Hasan Rakib",
+    age: 20,
+    country: "Bangladesh",
+    id: 1001,
+    isGraduated: false
+};
+
+const product = {
+    name: "Laptop",
+    price: 50000,
+    quantity: 5,
+    isAvailable: true
+};
+
+const profile = {
+    name: "Khairul Hasan Rakib",
+    age: 20,
+    country: "Bangladesh",
+    id: 1001,
+    isGraduated: false,
+    product: {
+        name: "Laptop",
+        price: 50000,
+        quantity: 5,
+        isAvailable: true
+    }
+};
