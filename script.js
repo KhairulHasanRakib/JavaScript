d16254e0f2570b42b66a5878ce0c3f61ee17b4d8
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2025-01-24 21:47:54 +0600

    edit

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-let gameNum = 25;
-let userNum = prompt("Guess The Game Number: ");
+// let gameNum = 25;
+// let userNum = prompt("Guess The Game Number: ");
 
-while (userNum != gameNum) {
-  userNum = prompt("You Entered The Wrong Number. Try Again: ");
-}
-console.log("You Guessed Right!");
+// while (userNum != gameNum) {
+//   userNum = prompt("You Entered The Wrong Number. Try Again: ");
+// }
+// console.log("You Guessed Right!");
 
-let books = {
-  book1: {
-    title: "The Alchemist",
-    author: "Paulo Coelho",
-    pages: 208,
-  },
-  book2: {
-    title: "The Great Gatsby",  
-    author: "F. Scott Fitzgerald",
-    pages: 180,
-  }
-}
+// let item = "Book";
+// let price = 50;
+
+// console.log(`The ${item} costs ${price} dollars.`);
+
+// let fname = prompt("Enter Your Full Name: ").trim();
+// console.log(`@${fname.toLowerCase().split(" ").join("")}${fname.length}`);
+
+let fname = prompt("Enter Your Full Name: ").replace(/\s+/g, '').trim();
+console.log(`@${fname.toLowerCase()}${fname.length}`);
