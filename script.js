173aab198e1d1046548378ebcff29a6b87e9b3a8
KhairulHasanRakib <khairulhasanrakib@gmail.com>
2025-01-29 17:57:11 +0600

    edit

--- script.js
+++ script.js
@@ -17,38 +17,59 @@
 // let fname = prompt("Enter Your Full Name: ").replace(/\s+/g, '').trim();
 // console.log(`@${fname.toLowerCase()}${fname.length}`);
 
-let cities = ["Istanbul", "Izmir", "Ankara", "Bursa", "Antalya"];
+// let cities = ["Istanbul", "Izmir", "Ankara", "Bursa", "Antalya"];
 // for loop
-for (let i = 0; i < cities.length; i++) {
-    console.log(cities[i]);
-}
+// for (let i = 0; i < cities.length; i++) {
+//     console.log(cities[i]);
+// }
 // for of loop
-for (let citi of cities) {
-    console.log(citi);
-}
+// for (let citi of cities) {
+//     console.log(citi);
+// }
 
 // find average of student marks [85, 97, 44, 37, 76, 60]
-let marks = [85, 97, 44, 37, 76, 60];
-let sum = 0;
-for (let mark of marks) {
-    sum += mark;
-}
-console.log(sum / marks.length);
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// for (let mark of marks) {
+//     sum += mark;
+// }
+// console.log(sum / marks.length);
 
 // 10% off for prices item
-let items = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-let i = 0;
-for (let val of items) {
-    let off = val / 10;
-    console.log(val - off);
-    i++;
-}
+// let items = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+// let i = 0;
+// for (let val of items) {
+//     let off = val / 10;
+//     console.log(val - off);
+//     i++;
+// }
 
-let company = ["Google", "Facebook", "Amazon", "Microsoft", "Apple"];
-console.log(company);
-company.shift();
-console.log(company);
-company.splice(3, 1, "Twitter");
-console.log(company);
-company.push("Oracle");
-console.log(company);
+// let company = ["Google", "Facebook", "Amazon", "Microsoft", "Apple"];
+// console.log(company);
+// company.shift();
+// console.log(company);
+// company.splice(3, 1, "Twitter");
+// console.log(company);
+// company.push("Oracle");
+// console.log(company);
+
+// Input string and print only vowels
+// let str = prompt("Enter a string: ");
+// let vowels = "aeiou";
+// for (let i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//         console.log(str[i]);
+//     }
+// }
+
+// In array print each number with there square
+let numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((numbers => {
+    console.log(numbers * numbers);
+}))
+
+let out = numbers.reduce((res, curr) => {
+    return res + curr;
+})
+console.log(out);
